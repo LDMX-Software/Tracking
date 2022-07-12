56884f23df1e17b724128fe306e771778e344556
EmrysPeets <65568285+EmrysPeets@users.noreply.github.com>
2022-07-12 13:47:54 -0700

    confif python script for validation processor

--- python/tracking_validate.py
+++ python/tracking_validate.py
@@ -1,32 +1,9 @@
 import os
-<<<<<<< Updated upstream
-from LDMX.Framework import ldmxcfg
-=======
 #from LDMX.Framework import ldmxcfg
->>>>>>> Stashed changes
 from LDMX.Framework.ldmxcfg import Producer
-
-
-#was from LDMX.Framework.ldmxcfg 
 
 #This class produces validation plots from a track collection.
 class validation_processor(Producer) :
 
     def __init__(self, instance_name = "validation_processor"):
         super().__init__(instance_name, 'tracking::reco::validation_processor','Tracking')
-
-
-#This class is to produce vertices between two track collections, i.e. for tagger/recoil matching for example.
-
-#class Vertexer(Producer) :
-#    def __init__(self, instance_name = "Vertexer"):
-#        super().__init__(instance_name,'tracking::reco::Vertexer','Tracking')
-<<<<<<< Updated upstream
-=======
-#from LDMX.Framework.ldmxcfg import Producer
-
-#class TrackingGeometryMaker(Producer) :
-
-#  def __init__(self, instance_name = 'TrackingGeometryMaker'):
-#    super().__init__(instance_name, 'tracking::sim::TrackingGeometryMaker', 'Tracking')
->>>>>>> Stashed changes
