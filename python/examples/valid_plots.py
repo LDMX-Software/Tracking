fb995121b164aa5e386f5905415ae93fb9e3f2ef
benjaminls <benjamin@lawrence-sanderson.com>
2022-07-13 14:24:45 -0700

    Fixed path variable issues

--- python/examples/valid_plots.py
+++ python/examples/valid_plots.py
@@ -1,11 +1,10 @@
 import os,math
 from LDMX.Framework import ldmxcfg
 
-sw_path = '/Users/benjaminlawrence-sanderson/workdir/projects/LDMX/sw/'
 
 p = ldmxcfg.Process("TrackerReco")
 p.libraries.append("libTracking.so")
-p.detector = os.path.join(sw_path, '/ldmx-sw/Detectors/data/ldmx-det-v12-dd4hep/detector.xml')
+p.detector = os.environ["LDMX_BASE"] + '/ldmx-sw/Detectors/data/ldmx-det-v12-dd4hep/detector.xml'
 
 
 from LDMX.Tracking import tracking_validate
@@ -27,10 +26,17 @@ print(p.sequence)
 
 
 p.inputFiles = [os.environ["LDMX_BASE"]+"/data_ldmx/single_e/single_ele_tagger.root"]
+p.inputFiles = [
+    # os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10001_t1636673834.root",
+    # os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10014_t1636673703.root",
+    # os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10014_t1636673701.root",
+    # os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10001_t1636674259.root",
+    # os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10001_t1636673835.root",
+    ]
 print(p.inputFiles)
 
 
-p.outputFiles = [os.path.join(sw_path, "data_ldmx/validation_plots.root")]
+p.outputFiles = [os.environ["LDMX_BASE"] + "/data_ldmx/validation_plots.root"]
 
 p.termLogLevel=0
 p.maxEvents = 1000
