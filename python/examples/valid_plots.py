3008f5937424cc34207739385ec4e56d9f2c2458
EmrysPeets <65568285+EmrysPeets@users.noreply.github.com>
2022-07-25 08:13:41 -0700

    an update on the plotting python script using the validation processor

--- python/examples/valid_plots.py
+++ python/examples/valid_plots.py
@@ -15,7 +15,7 @@ bugger = tracking_validate.validation_processor()
 bugger.dumpobj = 0
 
 #change to True for debugging
-bugger.debug = False
+bugger.debug = False 
 
 
 
@@ -25,11 +25,16 @@ p.sequence = [bugger]
 print(p.sequence)
 
 
-p.inputFiles = [os.environ["LDMX_BASE"]+"/data_ldmx/single_e/single_ele_tagger.root"]
+#p.inputFiles = [os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10014_t1636673701.root"]
+#p.inputFiles = [os.environ["LDMX_BASE"]+"/data_ldmx/single_e/mc_v12-4GeV-1e-inclusive_run10014_t1636673703.root"]
+p.inputFiles = [os.environ["LDMX_BASE"]+"/ldmx-sw/single_ele_tagger.root"]
 print(p.inputFiles)
 
 
+
+#p.outputFiles = ["/Users/emryspeets/sw/data_ldmx/validation_plots.root"]
 p.outputFiles = ["/Users/emryspeets/sw/data_ldmx/validation_plots.root"]
 
+
 p.termLogLevel=0
-p.maxEvents = 1000
+p.maxEvents = 50000
